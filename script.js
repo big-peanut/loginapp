1e5e9a69697d22b0b1660efc7ed6b8bc8845e5c2
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2023-06-03 13:02:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('myform');
+    const userList = document.getElementById('userlist');
+
+    // Function to display the list on page load
+    function displayListOnLoad() {
+        for (let i = 0; i < localStorage.length; i++) {
+            const email = localStorage.key(i);
+            displayDetails(email);
+        }
+    }
+
+    // Function to add user details to localStorage and display them
+    function addDetails(nameInput, emailInput, phoneInput) {
+        const user = {
+            name: nameInput.value,
+            email: emailInput.value,
+            phone: phoneInput.value
+        };
+
+        axios.post("https://crudcrud.com/api/33774df2b1364c7f942135b331f95bc0/appointmentDataNEW", user)
+            .then((res) => console.log(res.data.email))
+            .catch((err) => console.log(err))
+        displayDetails();
+    }
+
+    // Function to display user details
+    function displayDetails() {
+        axios.get("https://crudcrud.com/api/33774df2b1364c7f942135b331f95bc0/appointmentDataNEW")
+            .then((res) => {
+                for (var i = 0; i < res.data.length; i++) {
+                    const listItem = document.createElement('li');
+                    listItem.textContent = `Name: ${res.data[i].name}, Email: ${res.data[i].email}, Phone: ${res.data[i].phone}`;
+                    userList.appendChild(listItem);
+                    const deleteBtn = document.createElement('button');
+                    deleteBtn.textContent = 'Delete';
+                    deleteBtn.addEventListener('click', () => {
+                        deleteUser(res.data[i]._id, listItem);
+                    })
+                    listItem.appendChild(deleteBtn);
+                }
+            })
+            .catch((err) => console.log(err))
+    }
+
+    // Add submit event listener to the form
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const nameInput = document.getElementById('name');
+        const emailInput = document.getElementById('email');
+        const phoneInput = document.getElementById('phone');
+
+        addDetails(nameInput, emailInput, phoneInput);
+
+        nameInput.value = '';
+        emailInput.value = '';
+        phoneInput.value = '';
+    });
+
+    // Display the list on page load
+    displayListOnLoad();
+    function deleteUser(userId, listItem) {
+        axios
+            .delete(`https://crudcrud.com/api/33774df2b1364c7f942135b331f95bc0/appointmentDataNEW/${userId}`)
+            .then(() => {
+                userList.removeChild(listItem);
+            })
+            .catch((err) => console.log(err));
+    }
+});
